e930fc0218828e4f06f89587285f3e72b2a01a12
CristhianValencia23 <crisvalencia23v@gmail.com>
2025-03-16 21:31:42 -0500

    Create Footer.jsx

---
+++ public/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-8 p-4 bg-gray-200 text-center text-gray-700">
+      <p>&copy; 2025 EncuentraTodo. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
