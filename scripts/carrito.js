6eef3ae8e3632ce575e3b2a85d1386b7c17dbcd6
CristhianValencia23 <crisvalencia23v@gmail.com>
2025-04-21 01:00:04 -0500

    Delete scripts directory

--- scripts/carrito.js
+++
@@ -1,7 +0,0 @@
-
-function agregarAlCarrito(nombreProducto) {
-    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-    carrito.push({ nombre: nombreProducto, cantidad: 1 });
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    alert(`${nombreProducto} agregado al carrito`);
-}
