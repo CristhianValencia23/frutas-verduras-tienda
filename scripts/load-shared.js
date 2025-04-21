ca4747cb349b66ee5d6d5dd8838bb0a2e67b53db
CristhianValencia23 <crisvalencia23v@gmail.com>
2025-04-20 23:12:16 -0500

    Update load-shared.js

--- scripts/load-shared.js
+++ scripts/load-shared.js
@@ -1,1 +1,9 @@
-// Script de carga del header y footer
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('/shared/header.html')
+        .then(res => res.text())
+        .then(data => document.getElementById('header').innerHTML = data);
+
+    fetch('/shared/footer.html')
+        .then(res => res.text())
+        .then(data => document.getElementById('footer').innerHTML = data);
+});
